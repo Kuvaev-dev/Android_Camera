21edf66cf54977b3a0f7d167532f56fc60b64f34
Nikita Kuvaev <likeaboss13371@gmail.com>
2022-05-01 13:23:00 +0300

    Initial commit

---
+++ app/src/main/java/com/mainapp/cameraapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.mainapp.cameraapp;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+
+public class MainActivity extends FragmentActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
